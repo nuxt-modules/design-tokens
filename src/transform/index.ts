bbefe85e2a9515a9ef01aaea56bcfd8c41db96d7
Yaël Guilloux <yael.guilloux@gmail.com>
2022-08-30 13:25:20 +0200

    fix(transform): fix transform (replace all <style> tags)

--- src/transform/index.ts
+++ src/transform/index.ts
@@ -180,7 +180,7 @@ export const registerTransformPlugin = (nuxt: Nuxt, options: any) => {
 
 function transformVueSFC (code: string, id: string) {
   if (id.endsWith('.vue') && !id.includes('?')) {
-    const styleTagRe = /<style\b(.*?)\blang=['"][tj]sx?['"](.*?)>/
+    const styleTagRe = /<style\b(.*?)\blang=['"][tj]sx?['"](.*?)>/g
     if (code.match(styleTagRe)) {
       return code.replace(styleTagRe, '<style$1lang="postcss"$2>')
     }
